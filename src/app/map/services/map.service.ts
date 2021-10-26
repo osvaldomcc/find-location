f91500b778a803c6e43be2f9867cc6ca0d024547
osvaldomcc <ocastilloc152@gmail.com>
2021-10-27 00:49:52 +0100

    Add BehaviorSubject to watch location changes

--- src/app/map/services/map.service.ts
+++ src/app/map/services/map.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Icon, Map, Marker, TileLayer } from 'leaflet'
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { IpGeo } from '../interfaces/ipgeo.interface';
 import { tap } from 'rxjs/operators';
@@ -16,33 +16,30 @@ export class MapService {
   private _marker!: Marker;
   private _apiKey: string = environment.IP_GEO;
   private _baseUrl: string = environment.IP_GEO_BASE_URL;
-  private _userLocation!: IpGeo;
+  userLocation: BehaviorSubject<IpGeo> = new BehaviorSubject({});
 
   constructor(private http: HttpClient) { }
 
-  get userLocation() : Observable<IpGeo>{
-    return of(this._userLocation);
-  }
-
   //Inicializa el mapa
   initMap(mapRef: HTMLElement) : void{
-    const lat: number = Number(this._userLocation.latitude);
-    const lng: number = Number(this._userLocation.longitude);
     this._map = new Map(mapRef, {zoomControl: false});
-    this._map.setView([lat, lng], 13);
+    this.userLocation.subscribe( ({ latitude, longitude }) =>{
+      const lat: number = Number(latitude);
+      const lng: number = Number(longitude);
+      this._map.setView([lat, lng], 13);
+    } );
+    //Añade un nuevo marcador
+    this.addNewMarker();
     this._tileLayer = new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(this._map);
-    this._marker = new Marker([lat,lng]).setIcon(new Icon({
-      iconUrl: 'assets/visual/icon-location.svg',
-    })).addTo(this._map);
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this._map);        
   }
 
   //Obtiene la Locación del Ip del Usuario
   getUserLocation() : Observable<IpGeo> {
     const url = `${this._baseUrl}?apiKey=${this._apiKey}`;
     return this.http.get<IpGeo>(url).pipe(
-      tap( location => this._userLocation = location )
+      tap( location => this.userLocation.next(location) )
     );
   }
 
@@ -51,16 +48,23 @@ export class MapService {
     const url = `${this._baseUrl}?apiKey=${this._apiKey}&ip=${value}`;
     return this.http.get<IpGeo>(url).pipe(
       tap( location => {
-        this._userLocation = location;
-        const latitud: number = Number(location.latitude);
-        const longitud: number = Number(location.longitude);
-        this._map.setView([latitud, longitud], 13);
+        this.userLocation.next(location)
         this._map.removeLayer(this._marker);
-        this._marker = new Marker([latitud,longitud]).setIcon(new Icon({
-          iconUrl: 'assets/visual/icon-location.svg',
-        })).addTo(this._map);
+        //Añade un nuevo marcador al Mapa
+        this.addNewMarker();
       } ),
     );
   }
- 
+
+  //Añade un nuevo marcador al mapa
+  addNewMarker(){
+    this.userLocation.subscribe( ({ latitude, longitude}) => {
+      const lat: number = Number(latitude);
+      const lng: number = Number(longitude);
+      this._marker = new Marker([lat,lng]).setIcon(new Icon({
+        iconUrl: 'assets/visual/icon-location.svg',
+      })).addTo(this._map);
+    } )
+  }
+
 }
